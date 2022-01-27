b9d8aee268867f67b9e01386cda4493925ae6422
arleyguerrero70 <aguerrero0615@gmail.com>
2022-01-26 21:37:23 -0500

    Clase 6 POO-Básico --> Instancias de prototipos de objetos literales y de class

--- POO-Basico/index.js
+++ POO-Basico/index.js
@@ -62,3 +62,26 @@ const decimo = new Cursos(
 console.log(decimo);
 decimo.agregarMaterias('Ingles');
 console.log(decimo.materias);
+
+
+// Prototipos con la sintaxis de Clases
+
+class StudentClass{
+    constructor(name,age,courseAprove){
+        this.name = name;
+        this.age = age;
+        this.courseAprove = courseAprove;
+    }
+    newCourseAprove(nameCourse){
+        this.courseAprove.push(nameCourse);
+    }
+};
+
+const lizethClass = new StudentClass(
+    'Sheryll',
+    21,
+    ['Servicios farmaceuticos']
+);
+console.log(lizethClass);
+lizethClass.newCourseAprove('Jefe de enfermería');
+console.log(lizethClass);
